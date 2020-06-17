5a72e3d43fa4350606d71ec84847e0e7c5656cb9
evan yuanzh <yuanentai@yuanentaideiMac.local>
2020-06-17 17:23:48 +0800

    fix: worker

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-const createNetwork = (targetFun: Function) => () => {
+const createNetwork = (targetFun: Function) => {
   onmessage = (e: MessageEvent) => {
     const  { message } = e.data;
 
     const targetResult = targetFun(message);
 
-    postMessage({result: targetResult}, '*');
+    postMessage({result: targetResult});
   };
 }
 
 const initWorker = (targetFun: Function, callback: Function): Function => {
-  const target = createNetwork(targetFun);
-  const targetText = target.toString();
-  const IIFE = `(${targetText})()`;
+  const networkText = createNetwork.toString();
+  const IIFE = `(${networkText})(${targetFun.toString()})`;
   const blobData = new Blob([IIFE])
   const blobUrl = URL.createObjectURL(blobData);
   const worker = new Worker(blobUrl);
